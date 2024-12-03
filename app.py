fb8fbdeeec4eae45f27c1c902c0fa4857fbbd28b
Rama Diaz <ramadiaz221@gmail.com>
2024-12-03 18:52:10 +0700

    UPDATE of running host

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def predict_sign_language():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8014)
+    app.run(debug=True, host="0.0.0.0", port=8014)
