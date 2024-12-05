a641e6ac4b8eac7671b7113612bb515ddb1759a7
Rama Diaz <ramadiaz221@gmail.com>
2024-12-05 20:49:10 +0700

    UPDATE

--- app.py
+++ app.py
@@ -182,4 +182,4 @@ def predict_sign_language_video():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8014)
+    app.run(debug=True, host="0.0.0.0", port=4014)
